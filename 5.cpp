b696c94cfbea2ac78d6417bb2efbed91da53cf93
Pavankumardontha <pavankumardontha@gmail.com>
2021-08-30 16:49:16 +0530

    Create 5.cpp

---
+++ 5.cpp
@@ -0,0 +1,41 @@
+/*Creating threads in a loop*/
+#include <stdlib.h>
+#include <stdio.h>
+#include <pthread.h>
+
+/*We cannot create as many threads as we want.We have to optimize it based on our hardware. If we have a computer having 8 CPU cores then we can have 8 threads at
+the same time. */
+
+int mails = 0;
+pthread_mutex_t mutex;
+
+void* routine() {
+    for (int i = 0; i < 10000000; i++) {
+        pthread_mutex_lock(&mutex);
+        mails++;
+        pthread_mutex_unlock(&mutex);
+    }
+}
+
+int main(int argc, char* argv[]) 
+{
+    pthread_t th[8];
+    int i;
+    pthread_mutex_init(&mutex, NULL);
+    for (i = 0; i < 8; i++) {
+        if (pthread_create(th + i, NULL, &routine, NULL) != 0) {
+            perror("Failed to create thread");
+            return 1;
+        }
+        printf("Thread %d has started\n", i);
+    }
+    for (i = 0; i < 8; i++) {
+        if (pthread_join(th[i], NULL) != 0) {
+            return 2;
+        }
+        printf("Thread %d has finished execution\n", i);
+    }
+    pthread_mutex_destroy(&mutex);
+    printf("Number of mails: %d\n", mails);
+    return 0;
+}
